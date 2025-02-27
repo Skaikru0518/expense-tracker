7525943448fc7ad489957e1bcb04581d368bbe37
Skaikru0518 <dante0518@outlook.com>
2025-02-27 22:45:50 +0100

    initial commit

---
+++ frontend/expense-tracker/src/components/inputs/Input.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const Input = ({ value, onChange, placeholder, label, type }) => {
+	const [showPassword, setShowPassword] = useState(false);
+
+	const toggleShowPassword = () => {
+		setShowPassword(!showPassword);
+	};
+	return (
+		<div>
+			<label className="">{label}</label>
+			<div>
+				<input
+					type={
+						type == "password" ? (showPassword ? "text" : "password") : type
+					}
+					placeholder={placeholder}
+					className=""
+					value={value}
+					onChange={(e) => onChange(e)}
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default Input;
